85b85f0a86ca6127a04db5717aeae48b770d8062
mahesh giri <mahesh.irig77@gmail.com>
2019-01-30 11:35:35 -0600

    classwork done

--- classwork/02_assign/assign02.cpp
+++ classwork/02_assign/assign02.cpp
@@ -1,7 +1,7 @@
 //Write the function code that returns the product of hours and hourly_rate.
 #include "assign02.h"
-#include <iostream>
-using namespace std;
+
+
 
 double gross_pay(double hours, double hourly_rate)
 {
--- classwork/02_assign/main.cpp
+++ classwork/02_assign/main.cpp
@@ -1,30 +1,30 @@
 #include "assign02.h"
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::cin;
+using std::endl;
+
 int main() 
 {
 	//create a double variable named hours
 	//create a double variable named hourly_rate
+	double hours, hourly_rate, gross_salary;
 
 	//Prompt user for keyboard input for hours and hourly_rate
 	//save the input to variables hours and hourly_rate
+	cout << "Enter hours: " << endl;
+	cin >> hours;
+	cout << "Enter hourly rate: " << endl;
+	cin >> hourly_rate;
 
 	//Create a double variable named gross_salary
 	//Call the gross_pay function and save its return value to the gross_salary variable
+	gross_salary = gross_pay(hours, hourly_rate);
 
 	//Display the gross_salary variable as follows:
-	//Example ---->>>     Gross pay: 150.00   
-
-
-	
-		double hours, hourly_rate, total_pay;
-		cout << "Enter hours: " << endl;
-		cin >> hours;
-		cout << "Enter hourly rate: " << endl;
-		cin >> hourly_rate;
-
-		total_pay = gross_pay(hours, hourly_rate);
-		cout << "Gross pay is: " << total_pay << endl;
+	//Example ---->>>     Gross pay: 150.00  
+    
+	cout << "Gross pay is: " << gross_salary << endl;
 
 		return 0;
 	}
