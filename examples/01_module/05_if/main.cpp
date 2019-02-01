4d2ae2d48093e9de4f5c5249b9aeaae8f0bf2f79
8385207 <8385207@RRC2220-17>
2019-01-31 19:17:58 -0600

    Conditional structure examples and test cases

--- examples/01_module/05_if/main.cpp
+++ examples/01_module/05_if/main.cpp
@@ -1,16 +1,34 @@
-
+#include "if.h"
+#include<iostream>
+using std::cout;
+using std::cin;
+using std::endl;
 int main() 
 {
 	//create a bool variable named even
 	//create an int variable named value
 
+	bool even;
+	int value;
 	//prompt user for a keyboard number
 	//assign the number to the value variable
+	cout << "Enter the number: " << endl;
+	cin >> value;
 
 	//call the is_even function with value as its argument
 	//and assign its return value to the even variable
-
+	even = is_even(value);
 	//display whether the number is even or not
+	
+	if (even == true)
+	{
+		cout << value << " is even" << endl;
+	}
+
+	else
+	{
+		cout << value << " is odd" << endl;
+	}
 
 	return 0;
 }
