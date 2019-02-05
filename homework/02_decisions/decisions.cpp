af328673b9767619834879d8f7b6c34cb1e53b51
mahesh giri <mahesh.irig77@gmail.com>
2019-02-05 16:20:22 -0600

    Homework 2 done

--- homework/02_decisions/decisions.cpp
+++ homework/02_decisions/decisions.cpp
@@ -1,4 +1,6 @@
 //write include statement for decisions header
+#include "decisions.h"
+
 
 //Write code for function get_grade_points that accepts a string letter_grade and returns 
 //the grade_points for as follows:
@@ -8,7 +10,34 @@
 //given grade "D" returns 1
 //given grade "F" returns 0
 //another other value return -1
+int get_grade_points(string letter_grade)
+{
+	if (letter_grade == "A")
+	{
+		return 4;
+	}
 
+	else if (letter_grade == "B")
+	{
+		return 3;
+	}
+	else if (letter_grade == "C")
+	{
+		return 2;
+	}
+	else if (letter_grade == "D")
+	{
+		return 1;
+	}
+	else if (letter_grade == "F")
+	{
+		return 0;
+	}
+	else
+	{
+		return -1;
+	}
+}
 
 
 
@@ -18,4 +47,15 @@
 //a double named credit_points.  The function returns the quotient of credit_points divided by
 //credit_hours. In the function account for division by zero by returning a -1.
 
-
+double calculate_gpa(int credit_hours, double credit_points)
+{
+	if (credit_hours == 0)
+	{
+		return -1;
+	}
+	else
+	{
+		 return (credit_points / credit_hours);
+		
+	}
+}
--- homework/02_decisions/main.cpp
+++ homework/02_decisions/main.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
+#include "decisions.h"
+
 //write include statements
+using std::cout;
+using std::cin;
+using std::endl;
 
 /*
 In main write code to capture a letter_grade and credit_hours from the keyboard:
@@ -23,7 +28,48 @@ GPA 3.0
 */
 int main() 
 {
-	std::cout << "Hello" ;
+	string letter_grade;
+	int grade_points, credit_hours, credit_points;
+	int total_credit_hours = 0;
+	double total_credit_points = 0.0;
 
+	cout << "Please enter the letter grade for class 1 in uppercase: " << endl;
+	cin >> letter_grade;
+	grade_points = get_grade_points(letter_grade);
+
+	cout << "Please enter the credit hours for class 1: " << endl;
+	cin >> credit_hours;
+	total_credit_hours += credit_hours;
+
+	credit_points = grade_points * credit_hours;
+	total_credit_points += credit_points;
+
+	cout << "Please enter the letter grade for class 2 in uppercase: " << endl;
+	cin >> letter_grade;
+	grade_points = get_grade_points(letter_grade);
+
+	cout << "Please enter the credit hours for class 2: " << endl;
+	cin >> credit_hours;
+	total_credit_hours += credit_hours;
+
+	credit_points = grade_points * credit_hours;
+	total_credit_points += credit_points;
+
+	cout << "Please enter the letter grade for class 3 in uppercase: " << endl;
+	cin >> letter_grade;
+	grade_points = get_grade_points(letter_grade);
+
+	cout << "Please enter the credit hours for class 3: " << endl;
+	cin >> credit_hours;
+	total_credit_hours += credit_hours;
+
+	credit_points = grade_points * credit_hours;
+	total_credit_points += credit_points;
+
+	double gpa = calculate_gpa(total_credit_hours, total_credit_points);
+	cout <<"Your gpa is: "<< gpa << endl;
+
+	cin.get();
+	cin.ignore();
 	return 0;
 }
