6218df0b30f263998d3194f5305a8497d6d5ee4f
8385207 <8385207@RRC2220-17>
2019-05-07 19:06:07 -0500

    5/7 change

--- examples/12_module/06_arrays_char/main.cpp
+++ examples/12_module/06_arrays_char/main.cpp
@@ -1,3 +1,15 @@
+#include<iostream>
+using namespace std;
 int main()
 {
+	char name0[] = "John";
+	cout << name0 << std::endl;
+
+	for (int i = 0; name0[i] != '\0'; i++)
+	{
+		cout << name0[i];
+	}
+
+	char name1[5] = "Mary";
+	name1[4] = '\0';
 }
